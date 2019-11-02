0174fb6e0b1555a62de704996271212c3b728432
Nicolas  Poveda <nicolaspoveda@Air-de-Nicolas.mshome.net>
2019-11-01 21:48:52 -0500

    first commit

--- app/src/main/java/com/plasmadev/captiondad/carddashboard/MainActivity.java
+++ app/src/main/java/com/plasmadev/captiondad/carddashboard/MainActivity.java
@@ -1,7 +1,9 @@
 package com.plasmadev.captiondad.carddashboard;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,4 +12,12 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+    public void open(View v) {
+        Intent i = new Intent(this, Main2Activity.class);
+        this.startActivity(i);
+    }
+
+
+
 }
