3fc3f347e295c307082a45773ffc98202968e61e
Jose David Padilla Gamarra <djgamarra@uninorte.edu.co>
2019-11-02 12:20:54 -0500

    .

--- app/src/main/java/com/plasmadev/captiondad/carddashboard/ScanActivity.java
+++ app/src/main/java/com/plasmadev/captiondad/carddashboard/ScanActivity.java
@@ -3,7 +3,6 @@ package com.plasmadev.captiondad.carddashboard;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.util.Log;
 
 import com.google.firebase.firestore.DocumentSnapshot;
 import com.google.zxing.Result;
