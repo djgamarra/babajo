b43ce7b778b5e08d5399156b1c8a5e02f4f0a837
Jose David Padilla Gamarra <djgamarra@uninorte.edu.co>
2019-11-02 23:10:40 -0500

    .

--- app/src/main/java/com/plasmadev/captiondad/carddashboard/FirebaseHolder.java
+++ app/src/main/java/com/plasmadev/captiondad/carddashboard/FirebaseHolder.java
@@ -1,10 +1,13 @@
 package com.plasmadev.captiondad.carddashboard;
 
 import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
 
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.firestore.DocumentSnapshot;
 import com.google.firebase.firestore.FirebaseFirestore;
 import com.google.firebase.firestore.QuerySnapshot;
@@ -17,14 +20,22 @@ public class FirebaseHolder {
     public static List<DocumentSnapshot> historias = null;
     public static List<DocumentSnapshot> qrs = null;
     private static int loaded = 0;
+    @Nullable
+    private static FirebaseUser currentUser;
+    @Nullable
+    private static AuthListener authListener = null;
 
     public static FirebaseFirestore getInstance() {
         return FirebaseFirestore.getInstance();
     }
 
-    public static FirebaseAuth getAuthInstance() { return FirebaseAuth.getInstance(); }
+    public static FirebaseAuth getAuthInstance() {
+        return FirebaseAuth.getInstance();
+    }
 
-    public static boolean isAuth() { return getAuthInstance().getCurrentUser() != null; }
+    public static boolean isAuth() {
+        return getAuthInstance().getCurrentUser() != null;
+    }
 
     public static void init(final SplashScreen notif) {
         getInstance().collection("eventos").orderBy("fecha").get().addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
@@ -57,5 +68,29 @@ public class FirebaseHolder {
                 if (loaded == MODULES) notif.continuar();
             }
         });
+    }
+
+    public static void subscribe(AuthListener listener) {
+        authListener = listener;
+    }
+
+    public static void unSubscribe() {
+        authListener = null;
+    }
+
+    public static void login(String username, String password) {
+        getAuthInstance().signInWithEmailAndPassword(username, password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+                if (task.isSuccessful() && task.getResult() != null)
+                    currentUser = task.getResult().getUser();
+                else currentUser = null;
+                if (authListener != null) authListener.onChange(currentUser);
+            }
+        });
+    }
+
+    public interface AuthListener {
+        void onChange(FirebaseUser user);
     }
 }
--- app/src/main/java/com/plasmadev/captiondad/carddashboard/Login.java
+++ app/src/main/java/com/plasmadev/captiondad/carddashboard/Login.java
@@ -1,10 +1,14 @@
 package com.plasmadev.captiondad.carddashboard;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.widget.EditText;
 
-public class Login extends AppCompatActivity {
+import com.google.firebase.auth.FirebaseUser;
+
+public class Login extends AppCompatActivity implements FirebaseHolder.AuthListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -13,8 +17,30 @@ public class Login extends AppCompatActivity {
     }
 
     public void login(View v) {
-        
+        String email = ((EditText) this.findViewById(R.id.email)).getText().toString();
+        String pass = ((EditText) this.findViewById(R.id.password)).getText().toString();
+        FirebaseHolder.login(email, pass);
     }
 
+    public void signup(View v) {
+        Intent i = new Intent(this, SignUp.class);
+        this.startActivity(i);
+    }
 
+    @Override
+    protected void onPause() {
+        super.onPause();
+        FirebaseHolder.unSubscribe();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        FirebaseHolder.subscribe(this);
+    }
+
+    @Override
+    public void onChange(FirebaseUser user) {
+        this.finish();
+    }
 }
