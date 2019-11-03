504b93759aa5a5cc68abdf5d11763a82abc451ef
Nicolas  Poveda <nicolaspoveda@Air-de-Nicolas.mshome.net>
2019-11-02 19:19:30 -0500

    fixpls

--- app/src/main/java/com/plasmadev/captiondad/carddashboard/Ayuda.java
+++ app/src/main/java/com/plasmadev/captiondad/carddashboard/Ayuda.java
@@ -1,7 +1,10 @@
 package com.plasmadev.captiondad.carddashboard;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class Ayuda extends AppCompatActivity {
 
@@ -9,5 +12,32 @@ public class Ayuda extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_ayuda);
+    }
+
+
+    //replicado en clase Evento.java, cuidado al modificar este método
+    public void llamar(View v) {
+        Intent extras = this.getIntent();
+        String phone = "+57 3001237890";
+        if (phone != null) {
+            Uri u = Uri.parse("tel:" + phone);
+            Intent i = new Intent(Intent.ACTION_DIAL, u);
+            this.startActivity(i);
+        }
+    }
+
+    //replicado en clase Evento.java, cuidado al modificar este método
+    public void mail_to(View v) {
+        Intent extras = this.getIntent();
+        String email = "soporte@bajoelabonga.com";
+        if (email != null) {
+            Intent i = new Intent(Intent.ACTION_SENDTO);
+            i.setData(Uri.parse("mailto:"));
+            i.putExtra(Intent.EXTRA_EMAIL, email);
+            i.putExtra(Intent.EXTRA_SUBJECT, "");
+            if (i.resolveActivity(getPackageManager()) != null) {
+                startActivity(i);
+            }
+        }
     }
 }
