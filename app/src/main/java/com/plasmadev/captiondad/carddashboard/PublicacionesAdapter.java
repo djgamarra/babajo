4f1e1371ce0ff6a9c8104b3702990a898c600271
Jose David Padilla Gamarra <djgamarra@uninorte.edu.co>
2019-11-03 11:27:17 -0500

    .

--- app/src/main/java/com/plasmadev/captiondad/carddashboard/PublicacionesAdapter.java
+++ app/src/main/java/com/plasmadev/captiondad/carddashboard/PublicacionesAdapter.java
@@ -1,6 +1,7 @@
 package com.plasmadev.captiondad.carddashboard;
 
 import android.content.Context;
+import android.graphics.BitmapFactory;
 import android.support.annotation.NonNull;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
@@ -26,12 +27,12 @@ public class PublicacionesAdapter extends RecyclerView.Adapter<PublicacionesAdap
     @Override
     public PublicacionesViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int i) {
         View v = LayoutInflater.from(this.context).inflate(R.layout.publicacion, parent, false);
-        return new PublicacionesViewHolder(v, null, this.context);
+        return new PublicacionesViewHolder(v, this.r.get(i), this.context);
     }
 
     @Override
     public void onBindViewHolder(@NonNull PublicacionesViewHolder publicacionesViewHolder, int i) {
-        publicacionesViewHolder.setData(null);
+        publicacionesViewHolder.setData(this.r.get(i));
     }
 
     @Override
@@ -40,8 +41,8 @@ public class PublicacionesAdapter extends RecyclerView.Adapter<PublicacionesAdap
     }
 
     public static class PublicacionesViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener {
-        private TextView titulo, detalle, autor;
-        private ImageView imagen, icon;
+        private TextView titulo, detalle, autor, bacanos;
+        private ImageView imagen, icon, bacano;
         private DocumentSnapshot doc;
         private Context context;
 
@@ -49,11 +50,13 @@ public class PublicacionesAdapter extends RecyclerView.Adapter<PublicacionesAdap
             super(v);
             this.context = context;
             this.titulo = v.findViewById(R.id.feedTitle);
+            this.bacanos = v.findViewById(R.id.bacanos);
+            this.bacano = v.findViewById(R.id.bacano);
             this.detalle = v.findViewById(R.id.feedTextWall);
             this.autor = v.findViewById(R.id.feedUsername);
             this.imagen = v.findViewById(R.id.feedImage);
             this.icon = v.findViewById(R.id.feedUserIcon);
-            v.findViewById(R.id.eventoCard).setOnClickListener(this);
+//            v.findViewById(R.id.eventoCard).setOnClickListener(this);
             this.setData(d);
         }
 
@@ -62,8 +65,23 @@ public class PublicacionesAdapter extends RecyclerView.Adapter<PublicacionesAdap
             this.titulo.setText(doc.getString("nombre"));
             this.detalle.setText(doc.getString("detalle"));
             this.autor.setText(doc.getString("autor"));
+            int bacanos = Integer.parseInt(doc.get("bacanos").toString());
+            if (bacanos <= 0) this.bacanos.setText("");
+            else this.bacanos.setText(bacanos + " personas, qué bacanería!");
             this.imagen.setImageBitmap(Util.findOrCreateBy(doc.getString("imagen")));
             this.icon.setImageBitmap(Util.findOrCreateBy(doc.getString("icono")));
+            final ImageView b = this.bacano;
+            this.bacano.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    b.setOnClickListener(new View.OnClickListener() {
+                        @Override
+                        public void onClick(View view) {
+                            b.setImageBitmap(BitmapFactory.decodeResource(context.getResources(), R.drawable.bacanoiconfilled));
+                        }
+                    });
+                }
+            });
         }
 
         @Override
